7e2e7e0200b4ea457fb22faf88fe6ad7ef53cda8
Ichsan NA <49785290+dotnaonweh@users.noreply.github.com>
2022-01-11 21:33:47 +0700

    Update tiktok.py

--- tiktok.py
+++ tiktok.py
@@ -7,9 +7,9 @@ def tiktok(target):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Language': 'id,en-US;q=0.7,en;q=0.3',
-        'Alt-Used': 'savetiktok.cc',
+        'Alt-Used': 'savett.cc',
         'Connection': 'keep-alive',
-        'Referer': 'https://savetiktok.cc/en/download?url=',
+        'Referer': 'https://savett.cc/en/download?url=',
         'Upgrade-Insecure-Requests': '1',
         'Sec-Fetch-Dest': 'document',
         'Sec-Fetch-Mode': 'navigate',
@@ -23,7 +23,7 @@ def tiktok(target):
     elif 'tiktok.com/t/':
         response = requests.get(target).text
         regex = re.findall('content="(.*?)"', response)[-1].replace("&amp;", "&")
-        response1 = requests.get('https://savetiktok.cc/en/download?url='+regex, headers=headers).text
+        response1 = requests.get('https://savett.cc/en/download?url='+regex, headers=headers).text
         regex = re.findall('<option value="(.*?)"', response1)[0]
         video_url = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|)+', regex)[0].replace("&amp;", "&")
         res_video = requests.get(video_url, headers=headers)
@@ -31,7 +31,7 @@ def tiktok(target):
             fl.write(res_video.content)
         print("Download done, check folder Result")
     else:
-        response = requests.get('https://savetiktok.cc/en/download?url='+target, headers=headers).text
+        response = requests.get('https://savett.cc/en/download?url='+target, headers=headers).text
         regex = re.findall('<option value="(.*?)"', response)[0]
         video_url = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|)+', regex)[0].replace("&amp;", "&")
         res_video = requests.get(video_url, headers=headers)
