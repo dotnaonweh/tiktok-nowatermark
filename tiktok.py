7c930ca884d0117520267f2800754292b091a315
dotnaonweh <hellonudes66@gmail.com>
2022-08-01 23:20:37 +0700

    update api

--- tiktok.py
+++ tiktok.py
@@ -1,33 +1,55 @@
 import requests
 import re
 import random
+import os
+import sys
+import time
+
 
 def tiktok(target):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-        'Accept-Language': 'id,en-US;q=0.7,en;q=0.3',
-        'Alt-Used': 'savett.cc',
-        'Connection': 'keep-alive',
-        'Referer': 'https://savett.cc/en/download?url=',
-        'Upgrade-Insecure-Requests': '1',
-        'Sec-Fetch-Dest': 'document',
-        'Sec-Fetch-Mode': 'navigate',
-        'Sec-Fetch-Site': 'same-origin',
-        'Sec-Fetch-User': '?1',
+        'authority': 'api.app.downtik.com',
+        'accept': '*/*',
+        'accept-language': 'en-US,en;q=0.9',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'origin': 'https://downtik.com',
+        'referer': 'https://downtik.com/',
+        'sec-ch-ua': '" Not;A Brand";v="99", "Microsoft Edge";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-site',
+        'user-agent': 'Mozilla/5.0',
+    }
+
+    data = {
+        'url': target,
+        'lang': 'id',
     }
     filename = random.randrange(0, 1000)
-    if 'tiktok.com' not in target:
+    if 'tiktok.com' not in data['url']:
         print("Link Error")
     else:
-        response = requests.get('https://savett.cc/en/download?url='+target, headers=headers).text
-        regex = re.findall('<option value="(.*?)"', response)[0]
-        video_url = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|)+', regex)[0].replace("&amp;", "&")
-        res_video = requests.get(video_url, headers=headers)
+        response = requests.post(
+            'https://api.app.downtik.com/a.php', headers=headers, data=data).text
+        regex = re.findall('<a href="(.*?)"', response)[0]
+        res_video = requests.get(regex, headers=headers)
         with open("Result/tiktok-"+str(filename)+".mp4", 'wb') as fl:
             fl.write(res_video.content)
-        print("Download done, check folder Result")
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("")
+            print("                  Tiktok No Watermark Downloader")
+            print("")
+            animation = ["[■□□□□□□□□□] 10%", "[■■□□□□□□□□] 20%", "[■■■□□□□□□□] 30%", "[■■■■□□□□□□] 40%", "[■■■■■□□□□□] 50%",
+                         "[■■■■■■□□□□] 60%", "[■■■■■■■□□□] 70%", "[■■■■■■■■□□] 80%", "[■■■■■■■■■□] 90%", "[■■■■■■■■■■] 100%"]
+            for i in range(len(animation)):
+                time.sleep(0.5)
+                sys.stdout.write("\r[+] Downloading... " +
+                                 animation[i % len(animation)])
+                sys.stdout.flush()
+            print("\n[*] Download done, check folder Result.")
 
-x = input("Link Video > ")
-print('Wait....')
+
+x = input("[*] Link Video > ")
 tiktok(x)
